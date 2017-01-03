1e76fee0cfbfdc9951f6c49d1ba976921ecdb657
stelun <stephan.lunowa@web.de>
2017-01-03 18:46:44 +0100

    Corrected scripts to not interfer.

--- montecarlo.sh
+++ montecarlo.sh
@@ -5,9 +5,8 @@ DIR=Montecarlo$(date +%Y-%m-%d_%H-%M-%S)
 mkdir ${DIR}
 
 for I in $(seq 1 500); do
-  ./creator -rand 1500x166.666666666667 -tau 0.75 -dt 0.002 -iter 1000
-  ./numsim -I drivencavity &>${DIR}/run${I}.log
+  ./creator -o ${DIR} -rand 1500x166.666666666667 -tau 0.75 -dt 0.002 -iter 1000
+  ./numsim -I ${DIR} &>${DIR}/run${I}.log
 done
 
-rm -f drivencavity.geom drivencavity.param
-
+rm -f ${DIR}.geom ${DIR}.param
--- trapez.sh
+++ trapez.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-N=50
+N=200
 
 DIR=Trapez$(date +%Y-%m-%d_%H-%M-%S)
 mkdir ${DIR}
@@ -16,7 +16,7 @@ echo "Simulating Re from ${START} to ${END} in steps of ${STEP}"
 for n in $(seq 1 $N); do
   RE=$(bc <<< "${START} + $n * ${STEP}")
   ./creator -re ${RE} -tau 0.75 -dt 0.002 -iter 1000
-  ./numsim -I drivencavity &>${DIR}/run${I}.log
+  ./numsim -I drivencavity &>${DIR}/run${n}.log
 done
 
 rm -f drivencavity.geom drivencavity.param
