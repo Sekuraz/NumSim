d3ebecdcd215aa201737ed9b3c9f47c297e5fb28
Stephan Lunowa <stephan.lunowa@web.de>
2017-01-19 20:51:58 +0100

    Finished first analyse script for runtime.

--- solver_runtime/analyse.sh
+++ solver_runtime/analyse.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-REP=4
+REP=5
+
+rm -f drivencavity*.log drivencavity*.err drivencavity_*.csv
 
 for METHOD in CG MG RBSOR SOR; do
-  echo "  Method ${METHOD}:"
+  echo "Method ${METHOD}:"
   rm -f drivencavity_${METHOD}.csv
 
   for N in 8 16 32 64 #128
   do
-    echo "Size ${N}:"
+    echo "  Size ${N}:"
     FILE=drivencavity${N}
-    rm -f ${FILE}.log
     for i in $(seq 1 ${REP}); do
       echo "  run $i: /usr/bin/time -f\"${N} %e\" -ao ${FILE}.log "\
-          "../numsim -s ${METHOD} -I ${FILE} >/dev/null 2>${FILE}_${METHOD}.err"
+          "../numsim -s ${METHOD} -I ${FILE} >/dev/null 2>${FILE}_${METHOD}_${i}.err"
       /usr/bin/time -f"${N} %e" -ao ${FILE}_${METHOD}.log \
-          ../numsim -s ${METHOD} -I ${FILE} >/dev/null 2>${FILE}_${METHOD}.err
+          ../numsim -s ${METHOD} -I ${FILE} >/dev/null 2>${FILE}_${METHOD}_${i}.err
     done
     awk -F' ' '{k+=$1; sum+=$2;}; END { print k/NR, sum/NR, NR }' ${FILE}_${METHOD}.log >>drivencavity_${METHOD}.csv
   done
