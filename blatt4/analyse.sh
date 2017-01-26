5b723804679ed776b182fb0bbaa73caa86122093
Stephan Lunowa <stephan.lunowa@web.de>
2017-01-26 23:29:18 +0100

    Corrected analyse scripts of Blatt 4 (v4.0)

--- blatt4/analyse.sh
+++ blatt4/analyse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-awk -f mc_mean-sd.awk Montecarlo*/run*.log > mc_mean-sd_2000.csv
+awk -f mc_mean-sd.awk Montecarlo*/run{1..500}.log > mc_mean-sd_2000.csv
 awk -f mc_mean-sd.awk Montecarlo*/run{1..500..2}.log > mc_mean-sd_1000.csv
 awk -f mc_mean-sd.awk Montecarlo*/run{1..500..4}.log > mc_mean-sd_500.csv
 echo 'MC: mean-sd done'
@@ -11,12 +11,12 @@ echo 'MC: hist done'
 awk -f mc_conv.awk mc_hist.csv > mc_conv.csv
 echo 'MC: conv done'
 
-awk -f tr_mean-sd.awk Trapez*/run*.log > tr_mean-sd_200.csv
+awk -f tr_mean-sd.awk Trapez*/run{0..200}.log > tr_mean-sd_200.csv
 awk -f tr_mean-sd.awk Trapez*/run{0..200..2}.log > tr_mean-sd_100.csv
 awk -f tr_mean-sd.awk Trapez*/run{0..200..4}.log > tr_mean-sd_50.csv
 echo 'TR: mean-sd done'
 
-tail -qn 1 Trapez*/run*.log | awk -f tr_conv.awk > tr_conv.csv
+tail -qn 1 Trapez*/run{0..200}.log | awk -f tr_conv.awk > tr_conv.csv
 echo 'TR: conv done'
 
 pdflatex mc_analyse.tex
